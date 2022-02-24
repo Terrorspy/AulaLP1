5bba5155b75d18b12eb8eabfd00e6a12cb78af5a
Terrorspy <diogocaetanosoares922@gmail.com>
2022-02-24 15:43:49 +0000

    TiposVarios

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 3;
+            int x = 28;
+            int z = 8;
+            Console.WriteLine(i);
+            Console.WriteLine(x);
+            Console.WriteLine(z);
+        }
+    }
+}
