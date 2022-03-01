4ef3731d1088120f39c28f2693abde30e2a301ad
Terrorspy <diogocaetanosoares922@gmail.com>
2022-03-01 16:28:04 +0000

    VariasStrings

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         string s = "Um tab\t e um copyright\u03A9";
+         string d = @"Nao e’ um codigo Unicode \u1234, nao e’ nova linha \n";
+         Console.WriteLine(s);
+         Console.WriteLine(d);
+        }
+    }
+}
