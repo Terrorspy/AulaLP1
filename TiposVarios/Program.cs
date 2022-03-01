6c939758a09baacf80c9f0a8934c8962f36faac2
Terrorspy <diogocaetanosoares922@gmail.com>
2022-03-01 16:23:10 +0000

    TiposVarios

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -11,6 +11,14 @@ namespace TiposVarios
             int z = 8;
             char copyrightSymbol = '\u00A9';
             char newLine = '\u000A';
+            int a = 0x10;
+            uint j = 0x10U;
+            bool isCSharpFun = true;
+            bool isFishTasty = false;
+            Console.WriteLine(isCSharpFun);
+            Console.WriteLine(isFishTasty);
+            Console.WriteLine(a);
+            Console.WriteLine(j);
             Console.WriteLine(newLine);
             Console.WriteLine(copyrightSymbol);
             Console.WriteLine(i);
