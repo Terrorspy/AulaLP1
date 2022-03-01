583236336c7048594e4577ebe56f721d7e6972de
Terrorspy <diogocaetanosoares922@gmail.com>
2022-03-01 16:43:02 +0000

    VariasStrings

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -7,9 +7,16 @@ namespace VariasStrings
         static void Main(string[] args)
         {
          string s = "Um tab\t e um copyright\u03A9";
-         string d = @"Nao e’ um codigo Unicode \u1234, nao e’ nova linha \n";
+         string e = @"Nao e um codigo Unicode \u1234, nao e nova linha \n";
+         string a = "a" + 2;
+         string b = $"valor de x e";
+         string c = String.Format("Não Funciona??, please help");
+         Console.WriteLine(@"Verbatim com x = {0}");
+         Console.WriteLine(a);
+         Console.WriteLine(b);
          Console.WriteLine(s);
-         Console.WriteLine(d);
+         Console.WriteLine(e);
+         Console.WriteLine(c);
         }
     }
 }
